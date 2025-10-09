604745d07773eaa8773e0d8d2e9af2f8eddc6beb
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-10-09 11:49:13 -0400

    Create script.js

---
+++ Exercises/Num7/script.js
@@ -0,0 +1,8 @@
+console.log ("Hello, world!");
+console.log ("Lets start learning JavaScript");
+
+// String variable
+
+let studentName = "Billy Bob Joe";
+console.log (studentName);
+console.log ("The student's name is " + studentName);
