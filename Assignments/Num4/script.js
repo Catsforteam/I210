9f11ab5c3293525b770732f50e36a769a055d212
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-10-21 21:55:43 -0400

    aa

---
+++ Assignments/Num4/script.js
@@ -0,0 +1,20 @@
+let favoriteColor = 'Pink';
+console.log(favoriteColor);
+
+const birthYear = 2077;
+console.log(birthYear);
+
+let isStudent = true;
+console.log(isStudent);
+
+const person = { firstName: 'Patrick', lastName: 'Star' };
+console.log(person.firstName);
+
+let emptyValue = null;
+console.log(typeof emptyValue);
+
+console.log(typeof favoriteColor);
+console.log(typeof birthYear);
+console.log(typeof isStudent);
+console.log(typeof person);
+console.log(typeof emptyValue);
