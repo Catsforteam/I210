349da52691c5a239e418c1f30f1c113485c170ef
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-11-04 10:41:37 -0500

    Update script.js

--- Exercises/Num9/script.js
+++ Exercises/Num9/script.js
@@ -1,18 +1,15 @@
 fetch('student.json')
     .then(response => response.json())
-    .then( data => {
+    .then(data => {
         const container = document.getElementById('student-list');
 
-        data.students.forEach( student => {
+        data.students.forEach(student => {
 
             const p = document.createElement('p');
-            p.textContent = '$(Student.name)' ($(student.major)) - Grade: $(student.grade)';
+            p.textContent = `${student.name} (${student.major}) - Grade: ${student.grade}`;
 
             container.appendChild(p);
 
-        })
-
-        
+        });
     })
-
-    .catch(error => console.error('error: ', error))
+    .catch(error => console.error('Error:', error));
