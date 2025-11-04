490cc874fe552e8430677d68ff80827fa272fd18
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-11-04 11:09:05 -0500

    g

--- Exercises/Num10/script.js
+++ Exercises/Num10/script.js
@@ -3,15 +3,15 @@ let users = [];
 fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(data => {
-        // Store the fetched data in the users array
+        
         users = data;
-        // Function to display the user data
+        
         renderUsers(users);
     });
 
-// Define a function to display the user data
+
 function renderUsers(list) {
-    // Convert the array into HTML <li> elements using map()
+    
     userList.innerHTML = list.map(user => `
         <li>
             <strong>${user.name}</strong><br>
