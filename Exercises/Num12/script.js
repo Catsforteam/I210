544c3de6bf568b61010330652ced80a20de97cc7
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-11-13 11:24:59 -0500

    Update script.js

--- Exercises/Num12/script.js
+++ Exercises/Num12/script.js
@@ -9,3 +9,15 @@ const notesContainer = document.getElementById('notesContainer');
 const noteInput = document.getElementById('noteInput');
 
 const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click' , () =>
+{
+    const noteText = noteInput.value;
+
+    if(noteText.trim() !== '') return;
+
+    notesRef.push({
+        text: noteText,
+        timestamp: Date.now()
+    })
+})
