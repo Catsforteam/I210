7284fb93eff450949b28839723f853a30e7342ed
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-11-13 11:46:44 -0500

    u

--- Exercises/Num12/app.js
+++ Exercises/Num12/app.js
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQxETfI67in4wRXNKfpqf-fpglvUJmMjM",
+  authDomain: "notesapp-f31ba.firebaseapp.com",
+  databaseURL: "https://notesapp-f31ba-default-rtdb.firebaseio.com",
+  projectId: "notesapp-f31ba",
+  storageBucket: "notesapp-f31ba.firebasestorage.app",
+  messagingSenderId: "208358290960",
+  appId: "1:208358290960:web:82680959b55e1bfbf27c95",
+  measurementId: "G-EDP6Z0WC7L"
+};
+
 firebase.intializeApp(firebaseConfig);
 
 const database = firebase.database();
@@ -20,4 +31,6 @@ submitButton.addEventListener('click' , () =>
         text: noteText,
         timestamp: Date.now()
     })
+
+    noteInput.value = '';
 })
